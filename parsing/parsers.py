783ab3c631409594f74273c5fb79e9e19e58d606
Alessandro Silacci <alessandro.silacci@unil.ch>
2022-08-12 01:44:31 +0200

    Works partially for 2022 papers, if the title follows an element using the same font, the system goes crazy so be careful (or at least not suprised).

--- parsing/parsers.py
+++ parsing/parsers.py
@@ -91,7 +91,7 @@ class DocumentParser:
                                     if 'name' not in current_style.keys():
                                         current_style['name'] = char.fontname
                                         current_style['start'] = char_counter
-                                    elif char.fontname != current_style['name']:
+                                    elif char.fontname != current_style['name'] and token != ' ':
                                         current_style['end'] = char_counter
                                         if verbose:
                                             print(line_buffer[current_style['start']:current_style['end']],
@@ -112,15 +112,22 @@ class DocumentParser:
                                         elif line_buffer[-1:] != ' ':
                                             line_buffer += ' '
                                             char_counter += 1
+                    styles_stack.append(current_style)
         return line_buffer, styles_stack
 
     def parse(self, verbose=False):
+        """
+        Parses the document and returns a Document class containing the sections, sentences and titles.
+
+        :param verbose: print additional process information or not.
+        :return: an object, instance of the Document class (contains the sections with their titles and sentences).
+        """
         from .objects import Title, Sentence, Section, Document
         from os.path import basename
         from re import search, finditer
 
         document = Document(basename(self.document))
-        text, styles = self.__pdf_to_text(verbose=True)
+        text, styles = self.__pdf_to_text(verbose=verbose)
 
         line_buffer = ''
         sentences_buffer = []
@@ -162,95 +169,5 @@ class DocumentParser:
             if cur_start < len(line):
                 print(line[cur_start:len(line)-1], style, current_title)
                 line_buffer += line[cur_start:len(line)-1]
-
-        return document
-
-    def parse2(self, verbose=False):
-        """
-        Parses the document and returns a Document class containing the sections, sentences and titles.
-
-        :param verbose: print additional process information or not.
-        :return: an object, instance of the Document class (contains the sections with their titles and sentences).
-        """
-        from os.path import basename
-        from pdfminer.high_level import extract_pages
-        from pdfminer.layout import LTTextBoxHorizontal, LTTextLine, LTChar
-        from parsing.objects import Section, Sentence, Title, Document
-
-        pages = extract_pages(self.document)
-        if verbose:
-            print('####\nParsing Content in document {}\n####'.format(basename(self.document)))
-        document = Document(basename(self.document))
-
-        for page in pages:  # Hope you like indented code
-            for container in page:
-                if isinstance(container, LTTextBoxHorizontal):
-                    line_buffer = ''
-                    mapped_buffer = ''  # This might become a map with all the specific text encountered
-                    sentences = []
-                    mapped_type = ''  # might also become a map together with mapped_buffer
-                    mapped_accumulators = {}
-                    for line in container:
-                        if isinstance(line, LTTextLine):
-                            styles = {}
-                            for char in line:
-                                if isinstance(char, LTChar):
-                                    token = char.get_text()
-                                    if self.current_state is self.ParserState.LINE_END:
-                                        if len(line_buffer) >= 2 and line_buffer[-2:] == self.LINE_END_TOKEN \
-                                                and token.isupper():
-                                            sentences.append(Sentence(style=styles, content=line_buffer))
-                                            line_buffer = ''
-                                            self.current_state = self.ParserState.LINE
-                                    if char.fontname not in styles.keys():
-                                        styles[char.fontname] = 1
-                                    else:
-                                        styles[char.fontname] += 1
-                                    matched_mapping = False
-                                    for mapped_elt in self.map:
-                                        if char.fontname == mapped_elt['style'] or \
-                                           (token == ' ' and self.current_state == self.ParserState.MAP_MATCH):
-                                            mapped_buffer += token
-                                            mapped_type = mapped_elt['type']
-                                            if mapped_elt['type'] not in mapped_accumulators.keys():
-                                                mapped_accumulators[mapped_elt['type']] = ''
-                                            mapped_accumulators[mapped_elt['type']] += token
-                                            self.current_state = self.ParserState.MAP_MATCH
-                                            matched_mapping = True
-                                    if not matched_mapping and self.current_state != self.ParserState.LINE_END:
-                                        self.current_state = self.ParserState.LINE
-                                    if self.current_state == self.ParserState.LINE:
-                                        line_buffer += token
-                                        mapped_type = ''
-                                    if token == self.POTENTIAL_END_TOKEN:
-                                        print("line end detected", line_buffer)
-                                        self.current_state = self.ParserState.LINE_END
-                                elif isinstance(char, LTAnno):
-                                    if self.current_state != self.ParserState.MAP_MATCH:
-                                        if char.get_text() == ' ' and line_buffer[-1:] != ' ':
-                                            line_buffer += ' '
-                                        elif char.get_text() == '\n':
-                                            if len(line_buffer) > 1 and line_buffer[-1:] == '-':
-                                                line_buffer = line_buffer.rstrip(line_buffer[-1])
-                                            elif line_buffer[-1:] != ' ':
-                                                line_buffer += ' '
-                                    else:
-                                        if char.get_text() == ' ':
-                                            mapped_accumulators[mapped_type] += ' '
-                                        elif char.get_text() == '\n':
-                                            if len(line_buffer) > 1 and line_buffer[-1:] == '-':
-                                                line_buffer = line_buffer.rstrip(line_buffer[-1])
-                            if verbose:
-                                print('Page {page}: {line} --> {styles}'.format(page=page.pageid,
-                                                                                line=line.get_text(),
-                                                                                styles=styles))
-                    if len(line_buffer) > 0:
-                        sentences.append(Sentence(style=styles, content=line_buffer
-                                                  .translate(self.SUPPORTED_LIGATURES)))
-                    title = None
-                    if 'title' in mapped_accumulators.keys():
-                        title = mapped_accumulators['title']
-                    document.add_content(Section(title=Title(style=mapped_type, content=title),
-                                                 sentences=sentences))
 
         return document
