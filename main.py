9a302f017fe8260751832811e543f9289869e10d
Alessandro Silacci <alessandro.silacci@unil.ch>
2022-08-18 17:55:37 +0200

    Added progress bar indicator, created error check and correction mechanism.

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def start_parsing(filepath, mapfile, verbose=False):
     from os import listdir
     from parsing.parsers import DocumentParser
     from json import load
+    from progress.bar import ChargingBar
 
     files = []
 
@@ -37,8 +38,10 @@ def start_parsing(filepath, mapfile, verbose=False):
             map = load(f)
 
     parsers = []
-    for file in files:
-        parsers.append(DocumentParser(file, map).parse(verbose))
+    with ChargingBar('Parsing', max=len(files), suffix='%(index)d/%(max)d %(percent)d%%') as progress_bar:
+        for file in files:
+            parsers.append(DocumentParser(file, map).parse(verbose))
+            progress_bar.next()
 
     return parsers
 
@@ -61,7 +64,52 @@ def save_parsing_results(outputs, output_filepath='output/'):
                 dump(output.to_dict(), file, ensure_ascii=False)
 
 
+def check_results(output_folder, file=None):
+    from os import listdir
+
+    def check_file_content(filepath):
+        from json import load
+        data = None
+        with open(filepath, 'r') as res:
+            data = load(res)
+        return data is None or len(data['content']) == 0
+
+    file_issues = []
+    if file is not None and not check_file_content(output_folder + file):
+        print('Detected parsing issue with file: {filepath}'.format(filepath=output_folder + file))
+        file_issues.append(file)
+    else:
+        for f in listdir(output_folder):
+            if check_file_content(output_folder + f):
+                file_issues.append(f)
+                print('Detected parsing issue with file: {filepath}'.format(filepath=output_folder + f))
+
+    return file_issues
+
+
+def correct_map(filepath, mapfile, reference_word='ABSTRACT', type='title'):
+    from parsing.parsers import DocumentParser
+    from re import match
+    from json import load
+
+    with open(mapfile, 'r') as f:
+        map = load(f)
+
+    for f in filepath:
+        line, styles = DocumentParser(f, map).pdf_to_text()
+        for style in styles:
+            sub = line[style['start']:style['end']]
+            if match(reference_word, sub) is not None:
+                map.append({'type': type, 'style': style['name']})
+
+    return map
+
+
 if __name__ == '__main__':
+    from json import dump
+    with open('opening_lahari.txt', 'r') as opening:
+        opening_content = opening.read()
+        print(opening_content)
     args = init_arguments()
     if args.input is None:
         print('Bro, seriously... This code needs an input to run...')
@@ -69,3 +117,15 @@ if __name__ == '__main__':
     outputs = start_parsing(args.input, args.map, args.verbose)
     if args.output is not None:
         save_parsing_results(outputs, args.output)
+        file_errors = check_results(args.output)
+        new_files = []
+        for f in file_errors:
+            new_files.append(args.input + f.replace('.json', ''))
+
+        tentative_map = correct_map(new_files, args.map)
+        with open(args.map + '.extended', 'w+') as emap:
+            dump(tentative_map, emap)
+
+        for f in new_files:
+            out = start_parsing(f, args.map + '.extended')
+            save_parsing_results(out, args.output)
