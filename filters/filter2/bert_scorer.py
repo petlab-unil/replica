2041ca7e9aac1719daf6f35b491e134f66fa0b1d
poorao <poojarao126@gmail.com>
2022-08-30 14:59:32 +0200

    improvements to groundtruth

--- filters/filter2/bert_scorer.py
+++ filters/filter2/bert_scorer.py
@@ -3,7 +3,7 @@ from bert_score import BERTScorer
 import numpy as np
 
 
-def read_groundtruth(criteriafile):
+def read_json(criteriafile):
     """
     Reads the criteria file - criteria_goundtruth.json - containing minimum 5 groundtruth sentences per criteria
     :param: `criteriafile` (str): path to the criteria file
@@ -45,6 +45,7 @@ def calculate_sim_scores(filepath):
     from os import listdir
 
     files = []
+    test_files = []
 
     if isdir(filepath):
         files = [join(filepath, filename) for filename in listdir(filepath) if '.json' in filename]
@@ -53,24 +54,42 @@ def calculate_sim_scores(filepath):
     else:
         exit(1)
 
-    groundtruth = read_groundtruth("criteria_goundtruth.json") 
+    print (len(files))
+    exclusion = "Museum Internet Grindr tactoRing Experimental Super Bazaar Negotiations understandability Polite Tech Help Desk Automatically Generating OtherTube common Novice Video Consumption Privacy manipulated Commerce Everyday Modeling Brush Village See What Complementary".split(" ")
+    for ex in exclusion:
+        for json_file in files:
+            if (ex in basename(json_file)):
+                print ("*****************Skipping article {}\n".format(basename(json_file)), ex)
+                test_files.append(json_file)
+
+    print (len(test_files))
+    groundtruth = read_json("criteria_goundtruth.json") 
     scorer = BERTScorer(model_type="distilbert-base-uncased")
+    threshold = read_json("threshold_scores.json")
     
-    dfs = []
-    for json_file in files[0:1]:
+    scores = []
+    predictions = []
+    
+    for json_file in test_files:
         sentences = json_to_sent(json_file)
         print('####\nProcessing article {}\n'.format(basename(json_file)))
 
+        paper_prediction = {}
         for key in groundtruth:
             print('----\nChecking criteria {}\n----'.format(key))
             p, r, f = scorer.score(sentences, [groundtruth[key]]*len(sentences))
-            mask = f > 0.75
+            mask = f > threshold[key]
             indices = mask.nonzero().flatten().numpy()
-            df = pd.DataFrame({'criteria': [key]*len(indices), 'sentences': np.array(sentences)[indices], 'scores': f[indices], 'paper_title': [basename(json_file)]*len(indices)})
-            dfs.append(df)
-    final = pd.concat(dfs)
-    final.to_csv("final.csv")
+            df_scores = pd.DataFrame({'criteria': [key]*len(indices), 'sentences': np.array(sentences)[indices], 'scores': f[indices], 'groundtruth': [groundtruth[key]]*len(indices), 'paper_title': [basename(json_file)]*len(indices)})
+            paper_prediction[key] = [int(any(indices))]
+            scores.append(df_scores)
+        paper_prediction['paper_title'] = [basename(json_file)]
+        predictions.append(pd.DataFrame(paper_prediction))
+    final = pd.concat(scores)
+    pred = pd.concat(predictions)
+    pred.to_csv("predictions.csv")
+    final.to_csv("final_test.csv")
 
 
 if __name__ == '__main__':
-    calculate_sim_scores("output/pilot/2017")
+    calculate_sim_scores("output/")
