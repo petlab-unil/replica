01909920234ddb9f4612a5584ebd8c9fdfbe3e51
poorao <poojarao126@gmail.com>
2023-01-04 10:25:54 +0100

    updated documentation

--- filters/filter2/bert_scorer.py
+++ filters/filter2/bert_scorer.py
@@ -1,13 +1,20 @@
 from json import load
+from os.path import isdir, basename, join
+from os import listdir
+
+import numpy as np
+import pandas as pd
 from bert_score import BERTScorer
 from transformers import pipeline
-import numpy as np
+
 
 
 def read_json(criteriafile):
     """
-    Reads the criteria file - criteria_goundtruth.json - containing minimum 5 groundtruth sentences per criteria
+    Reads the json file
+
     :param: `criteriafile` (str): path to the criteria file
+
     :return: `data` (dict): a dict of criteria and sentences
     """
     with open(criteriafile, 'r') as f:
@@ -18,8 +25,10 @@ def read_json(criteriafile):
 def json_to_sent(json_file):
     """
     Converts PDF parser output to list of sentences with greater than two words
+
     :param: `json_file` (str): path to the json output file from the PDF parser
-    :return: `sentences` (list): list of sentences from the PDF 
+
+    :return: `sentences` (list of str): list of sentences from the PDF 
     """
 
     from nltk.tokenize import sent_tokenize
@@ -32,8 +41,8 @@ def json_to_sent(json_file):
             for sentence in sent_tokenize(sent['content']):
                 if len(sentence.split(' ')) > 2: 
                     sentences.append(sentence)
-
-    if len(sentences)==0:
+    # some of the json generated only few sentences (here assumed < 100) in the sentence nodes, hence checking title nodes
+    if len(sentences)<100:
         print ("Empty sentence, checking title nodes:", sentences)
         for para in data['content']:
             for sentence in sent_tokenize(para['title']['content']):
@@ -43,28 +52,58 @@ def json_to_sent(json_file):
     return sentences
 
 def classify_criteria(classifier, sentences, criteria):
+    """
+    NLI based zero-shot classification which calculates the *entailment* probability between sentences and criteria 
+    template sentence "this is an example of <keyword>"
+
+    :param: `classifier` (obj): NLI-based zero-shot classification pipeline object
+    :param: `sentences` (list of string): list of sentences 
+    :param: `criteria` (list of string): list of keywords for the criterion considered as candidate labels
+
+    :return: `results` (dict): a dict with the following keys:
+            - **sequence** (`str`) -- the sequence for which this is the output
+            - **labels** (`List[str]`) -- the keywords or labels sorted by order of likelihood
+            - **scores** (`List[float]`) -- the probabilities for each of the keywords 
+    """
     results = classifier(sentences, criteria, multi_label=True)
     return results
      
 def calculate_sim_scores(scorer, sentences, groundtruth, threshold, criteria):
+    """
+    Calculates the BERTscore of similarity between the paper sentences and reference sentences
+
+    :param: `scorer` (obj): BERTScorer object 
+    :param: `sentences` (list of string): list of sentences from the PDF
+    :param: `groundtruth` (list of string): list of reference sentences for the criterion
+    :param: `threshold` (float): threshold score for the criterion
+    :param: `criteria` (str): the criterion
+
+    :return: `results` (dict): a dict containing the criterion, sentences whose similarity scores are higher than the 
+                                threshold and their respective scores
+    """
     p, r, f = scorer.score(sentences, [groundtruth]*len(sentences))
-    # mask = f > 0.75
     mask = f > threshold
     indices = mask.nonzero().flatten().numpy()
     results = {'criteria': [criteria]*len(indices), 'sentences': list(np.array(sentences)[indices]), 'sim_scores': f[indices]}
     return results
 
 
-def check_criteria(filepath, use_bert_score = True, use_zero_shot_classifier = True):
+def check_criteria(filepath, use_sim_score = True, use_zero_shot_classifier = True):
     """
-    Calculates similarity scores between the groundtruth sentences and all the sentences of the PDF for each criteria. 
-    Threshold for similarity score is empirically set at different levels for each criteria.
-    Writes output to a .csv file with columns criteria, sentence, score, paper title
+    Calls (optionally) the modules of similarity score filter and zero-shot classifier to check criteria satisfaction 
+
     :param: `filepath` (str): path to the folder containing the outputs of the PDF parser or a json output file
+    :param: `use_sim_score` (bool): a boolean specifying whether to use similarity score filter or not
+    :param: `use_zero_shot_classifier` (bool): a boolean specifying whether to use zero-shot classifier or not
+
+    Writes output to two .csv files -
+    - sentences.csv with columns criteria, sentence, similarity score, labels, probability scores, paper title, max probability score
+    - predictions.csv with one column for each criteria and paper title
     """
-    import pandas as pd
-    from os.path import isdir, basename, join
-    from os import listdir
+
+    assert (
+            use_sim_score is not False or use_zero_shot_classifier is not False
+        ), "Either of use_sim_score or use_zero_shot_classifier should be True"
 
     files = []
     test_files = []
@@ -75,52 +114,57 @@ def check_criteria(filepath, use_bert_score = True, use_zero_shot_classifier = T
         files.append(filepath)
     else:
         exit(1)
+    
+    # read the reference sentences
+    groundtruth = read_json("criteria_goundtruth.json")  
 
-    print (len(files))
-    # exclusion = "Museum Internet Grindr tactoRing Experimental Super Bazaar Negotiations understandability Polite Tech Automatically OtherTube common Novice Video Privacy manipulated Commerce Everyday Curved Brush Village What Complementary".split(" ")
-    # for ex in exclusion:
-    #     for json_file in files:
-    #         if (ex in basename(json_file)):
-    #             print ("*****************Skipping article {}\n".format(basename(json_file)), ex)
-    #             test_files.append(json_file)
-
-    # print (len(test_files))
-    groundtruth = read_json("criteria_goundtruth.json") 
-    if use_bert_score: scorer = BERTScorer(model_type="distilbert-base-uncased")
+    # list of sentences used to compute the idf weights, providing all reference sentences for all criteria to keep it simple
+    all_gt = []
+    for key in groundtruth['sim_matcher'][0]: all_gt.extend(groundtruth["sim_matcher"][0][key])
+    
+    if use_sim_score: scorer = BERTScorer(model_type="distilbert-base-uncased", idf=True, idf_sents=all_gt)
     if use_zero_shot_classifier: classifier = pipeline("zero-shot-classification", device=0)
+    
+    # read the threshold scores for similarity filter, different for each criteria
     threshold = read_json("threshold_scores.json")
     
     scores = []
     predictions = []
+    # entailment probability threshold was empirically determined to be 0.78
+    threshold_prob = 0.78
     
     for json_file in files:
         print('####\nProcessing article {}\n'.format(basename(json_file))) 
         sentences = json_to_sent(json_file)
 
         paper_prediction = {}
-        # for key in groundtruth["zero_shot"][0]:
-        for key in ["Pre-registration", "Sample size for QuaN", "Anonymization"]:
+        for key in groundtruth["zero_shot"][0]:
             print('Checking criteria {}\n----'.format(key))
 
-            if use_bert_score:
+            if use_sim_score:
                 sim_results = calculate_sim_scores(scorer, sentences, groundtruth["sim_matcher"][0][key], threshold["zero_shot"][0][key], key)
-            if len(sim_results['sentences'])>0:
-                if use_zero_shot_classifier:
+
+            if use_zero_shot_classifier:
+                if len(sim_results['sentences'])>0:
                     results = classify_criteria(classifier, sim_results['sentences'], groundtruth['zero_shot'][0][key])
+
+                # concatenating the results from similarity filter and classifier in a dataframe
                 df_scores = pd.concat([pd.DataFrame(sim_results), pd.DataFrame(results)], axis=1)
                 df_scores['paper_title'] = [basename(json_file)]*len(df_scores)
                 df_scores['max_label_score'] = df_scores['scores'].apply(lambda x:x[0])
-                paper_prediction[key] = [int(any(df_scores['max_label_score'] > 0.80))]
+                
+                # if any of the sentence crosses the threshold probability, prediction is marked as 1 for that paper title and criteria
+                paper_prediction[key] = [int(any(df_scores['max_label_score'] > threshold_prob))]
                 scores.append(df_scores)
         paper_prediction['paper_title'] = [basename(json_file)]
         predictions.append(pd.DataFrame(paper_prediction))
+
     final = pd.concat(scores)
     pred = pd.concat(predictions)
     pred.fillna(0, inplace=True)
-    pred.to_csv("final_predictions_zero_shot5.csv")
-    final.to_csv("final_sentences_zero_shot5.csv")
+    pred.to_csv("predictions.csv")
+    final.to_csv("sentences.csv")
 
 
 if __name__ == '__main__':
     check_criteria("parserOutput-all")
-    # check_criteria("output")
